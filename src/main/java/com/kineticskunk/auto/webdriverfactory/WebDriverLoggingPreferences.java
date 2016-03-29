c9b7d6a26882289045786803a0922c94fed752e4
Donovan Mulder <donovanmulder@kineticskunk.com>
2016-03-29 07:20:29 +0200

    Modified WebDriverLoggingPerference class: Add constructor with no parameters Add methods: 1. setBrowserLogLevel 2. setClientLogLevel 3. setDriverLogLevel 4. setPerformanceLogLevel 5. setProfileLogLevel 6. setServerLogLevel

--- src/main/java/com/kineticskunk/auto/webdriverfactory/WebDriverLoggingPreferences.java
+++ src/main/java/com/kineticskunk/auto/webdriverfactory/WebDriverLoggingPreferences.java
@@ -1,15 +1,19 @@
 package com.kineticskunk.auto.webdriverfactory;
 
 import java.util.logging.Level;
-
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 
 public class WebDriverLoggingPreferences {
 
-	private LoggingPreferences logs = new LoggingPreferences();
+	private LoggingPreferences logs;
+	
+	public WebDriverLoggingPreferences() {
+		this.logs = new LoggingPreferences();
+	}
 
-	private WebDriverLoggingPreferences (String loggingLevel) {
+	public WebDriverLoggingPreferences (String loggingLevel) {
+		this();
 		this.logs.enable(LogType.BROWSER, this.getLevel(loggingLevel));
 		this.logs.enable(LogType.CLIENT, this.getLevel(loggingLevel));
 		this.logs.enable(LogType.DRIVER, this.getLevel(loggingLevel));
@@ -42,9 +46,32 @@ public class WebDriverLoggingPreferences {
 			return Level.ALL;
 		}
 	}
+	
+	public void setBrowserLogLevel(String loggingLevel) {
+		this.logs.enable(LogType.BROWSER, this.getLevel(loggingLevel));
+	}
+	
+	public void setClientLogLevel(String loggingLevel) {
+		this.logs.enable(LogType.CLIENT, this.getLevel(loggingLevel));
+	}
+	
+	public void setDriverLogLevel(String loggingLevel) {
+		this.logs.enable(LogType.DRIVER, this.getLevel(loggingLevel));
+	}
+	
+	public void setPerformanceLogLevel(String loggingLevel) {
+		this.logs.enable(LogType.PERFORMANCE, this.getLevel(loggingLevel));
+	}
+	
+	public void setProfileLogLevel(String loggingLevel) {
+		this.logs.enable(LogType.PROFILER, this.getLevel(loggingLevel));
+	}
+	
+	public void setServerLogLevel(String loggingLevel) {
+		this.logs.enable(LogType.SERVER, this.getLevel(loggingLevel));
+	}
 
 	public LoggingPreferences getLoggingPreferences() {
 		return logs;
 	}
 }
-
