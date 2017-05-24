5684378b5de195b04a782bf5d9529721994c4b28
Donovan Mulder <donovan.mulder@kineticskunk.com>
2017-05-24 15:40:56 +0200

    Added code to handle OperaDriver in DriverExecutable.setDriverExecutable

--- src/main/java/com/kineticskunk/driverfactory/DriverExecutable.java
+++ src/main/java/com/kineticskunk/driverfactory/DriverExecutable.java
@@ -21,6 +21,12 @@ public class DriverExecutable {
 	private static final String CHROMEDRIVERWINDOWS64 = "chromedriver-v2.29-win32.exe";
 	private static final String CHROMEDRIVERWINDOWS32 = "chromedriver-v2.29-win32.exe";
 	
+	private static final String OPERADRIVERMAC64 = "operadriver-v2.27-mac64";
+	private static final String OPERADRIVERLINUX64 = "operadriver-v2.27-linux64";
+	private static final String OPERADRIVERLINUX32 = "operadriver-v2.27-linux32";
+	private static final String OPERADRIVERWINDOWS64 = "operadriver-v2.27-win64.exe";
+	private static final String OPERADRIVERWINDOWS32 = "operadriver-v2.27-win32.exe";
+	
 	private PlatformOperatingSystem pos = new PlatformOperatingSystem();
 	
 	private String browserType = null;
@@ -49,7 +55,6 @@ public class DriverExecutable {
 		case "chrome":
 			if (this.pos.isMac() && System.getProperty("os.arch").contains("64")) {
 				driverExecutable = new File(this.getClass().getClassLoader().getResource(CHROMEDRIVERMAC64).getPath());
-				
 			} else if (this.pos.isUnix() && System.getProperty("os.arch").contains("64")) {
 				this.driverExecutable = new File(this.getClass().getClassLoader().getResource(CHROMEDRIVERLINUX64).getPath());
 			} else if (this.pos.isWindows()) {
@@ -62,13 +67,27 @@ public class DriverExecutable {
 			System.setProperty("webdriver.chrome.driver", this.driverExecutable.getAbsolutePath());
 			break;
 		case "opera":
-			
+			if (this.pos.isMac() && System.getProperty("os.arch").contains("64")) {
+				driverExecutable = new File(this.getClass().getClassLoader().getResource(OPERADRIVERMAC64).getPath());
+			} else if (this.pos.isUnix()) {
+				if (System.getProperty("os.arch").contains("64")) {
+					this.driverExecutable = new File(this.getClass().getClassLoader().getResource(OPERADRIVERLINUX64).getPath());
+				} else if (System.getProperty("os.arch").contains("32")) {
+					this.driverExecutable = new File(this.getClass().getClassLoader().getResource(OPERADRIVERLINUX32).getPath());
+				}
+			} if (this.pos.isWindows()) {
+				if (System.getProperty("os.arch").contains("64")) {
+					this.driverExecutable = new File(this.getClass().getClassLoader().getResource(OPERADRIVERWINDOWS64).getPath());
+				} else if (System.getProperty("os.arch").contains("32")) {
+					this.driverExecutable = new File(this.getClass().getClassLoader().getResource(OPERADRIVERWINDOWS32).getPath());
+				}
+			}
+			System.setProperty("webdriver.opera.driver", this.driverExecutable.getAbsolutePath());
+			break;
 		}
 		this.makeDriverExecutable(this.driverExecutable.getAbsolutePath());
 	}
 	
-	//System.setProperty("webdriver.firefox.bin","/Users/yodaqua/ks-test-automation/firefox-lib/Firefox_53.app/Contents/MacOS/firefox-bin");
-
 	private void makeDriverExecutable(String driverFile) {
 		CLibrary libc = (CLibrary) Native.loadLibrary("c", CLibrary.class);
 		libc.chmod(driverFile, 0755);
